66e4b60bd6c833cbe94f03973c5dd06c8b715d8a
Noah Connors <noahconnors@mnmsa.org>
2019-01-07 18:11:25 -0600

    Clean up the sequential command
    Doesn't call all inits at once
    Is safe to pass an empty array to the constructor

--- src/main/java/frc/command/SequentialCommand.java
+++ src/main/java/frc/command/SequentialCommand.java
@@ -7,33 +7,38 @@ package frc.command;
 public class SequentialCommand implements Command {
     private final Command[] commands;
     private int index = 0;
+    private boolean isEmpty;
 
     /**
      * @param commands an array of the commands to be run sequentially
      */
     public SequentialCommand(Command[] commands) {
         this.commands = commands;
+        isEmpty = commands.length == 0;
     }
 
     public void init() {
-        for(Command command : commands) {
-            command.init();
+        if(!isEmpty) {
+            commands[0].init();
         }
     }
 
     public void execute() {
-        if(commands[index].isFinished()) {
-            commands[index].end();
-            if(index < commands.length - 1) {
-                index += 1;
+        if(!isEmpty) {
+            if(commands[index].isFinished()) {
+                commands[index].end();
+                if(index < commands.length - 1) {
+                    index += 1;
+                    commands[index].init();
+                }
+            } else {
+                commands[index].execute();
             }
-        } else {
-            commands[index].execute();
         }
     }
 
     public boolean isFinished() {
-        return commands[commands.length - 1].isFinished();
+        return isEmpty ? true : commands[commands.length - 1].isFinished();
     }
 
     public void end() {
