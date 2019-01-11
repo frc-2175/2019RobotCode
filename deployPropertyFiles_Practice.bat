9a3412442ebad6d81c02e3aae97be153c1ae262c
Noah Connors <noahconnors@mnmsa.org>
2019-01-10 18:20:51 -0600

    Add property files as well as ways to deploy them

---
+++ deployPropertyFiles_Practice.bat
@@ -0,0 +1,6 @@
+::Shared Files
+pscp -pw "" src\properties\*.properties admin@roborio-2175-frc.local:/home/lvuser
+::Bot Specified Files
+pscp -pw "" src\properties\practiceBot\*.properties admin@roborio-2175-frc.local:/home/lvuser
+::SSH files to robot
+plink -ssh -pw "" admin@roborio-2175-frc.local "mkdir -m 775 -v -p log; . /etc/profile.d/natinst-path.sh; /usr/local/frc/bin/frcKillRobot.sh -t -r"
---
+++ deployPropertyFiles_Practice.sh
@@ -0,0 +1,6 @@
+# Shared Files
+scp src\properties\*.properties admin@roborio-2175-frc.local:/home/lvuser
+# Bot Specified Files
+scp src\properties\practiceBot\*.properties admin@roborio-2175-frc.local:/home/lvuser
+# SSH files to robot
+ssh admin@roborio-2175-frc.local "mkdir -m 775 -v -p log; killall -q netconsole-host || :"
