4804eef01a40a170e2b0b26e3ce7ffe16b6e8067
Noah Connors <noahconnors@mnmsa.org>
2019-01-10 18:20:19 -0600

    Add SmartDashboardInfo from last year's code

---
+++ src/main/java/frc/info/SmartDashboardInfo.java
@@ -0,0 +1,85 @@
+package frc.info;
+
+import java.util.Properties;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class SmartDashboardInfo {
+    private static final String PREFIX = "AutoPopulate/";
+    private final boolean isComp;
+    
+    public SmartDashboardInfo() {
+        Properties properties = RobotInfo.loadProperties("/home/lvuser/bot.properties");
+        isComp = Boolean.parseBoolean((String) properties.get("isComp"));
+        populate();
+    }
+
+    /**
+     * This is where all smart dashboard values are put onto the dashboard
+     * using the put methods. See below in {@link SmartDashboardInfo}
+     */
+    public void populate() {
+
+    }
+
+    public void putBoolean(String key, boolean comp, boolean practice) {
+		SmartDashboard.putBoolean(PREFIX + key, isComp ? comp : practice);
+	}
+
+	public void putNumber(String key, double comp, double practice) {
+		SmartDashboard.putNumber(PREFIX + key, isComp ? comp : practice);
+	}
+
+	public void putString(String key, String comp, String practice) {
+		SmartDashboard.putString(PREFIX + key, isComp ? comp : practice);
+	}
+
+	public void putBooleanArray(String key, boolean[] comp, boolean[] practice) {
+		SmartDashboard.putBooleanArray(PREFIX + key, isComp ? comp : practice);
+	}
+
+	public void putNumberArray(String key, double[] comp, double[] practice) {
+		SmartDashboard.putNumberArray(PREFIX + key, isComp ? comp : practice);
+	}
+
+	public void putStringArray(String key, String[] comp, String[] practice) {
+		SmartDashboard.putStringArray(PREFIX + key, isComp ? comp : practice);
+	}
+
+	public void putRaw(String key, byte[] comp, byte[] practice) {
+		SmartDashboard.putRaw(PREFIX + key, isComp ? comp : practice);
+	}
+
+	public boolean getBoolean(String key) {
+		return SmartDashboard.getBoolean(PREFIX + key, false);
+	}
+
+	public double getNumber(String key) {
+		return SmartDashboard.getNumber(PREFIX + key, 0);
+	}
+
+	public String getString(String key) {
+		return SmartDashboard.getString(PREFIX + key, "");
+	}
+
+	public boolean[] getBooleanArray(String key) {
+		boolean[] defaultArray = { false };
+		return SmartDashboard.getBooleanArray(PREFIX + key, defaultArray);
+	}
+
+	public double[] getNumberArray(String key) {
+		double[] defaultArray = { 0 };
+		return SmartDashboard.getNumberArray(PREFIX + key, defaultArray);
+	}
+
+	public String[] getStringArray(String key) {
+		String[] defaultArray = { "" };
+		return SmartDashboard.getStringArray(PREFIX + key, defaultArray);
+	}
+
+	public byte[] getRaw(String key) {
+		byte[] defaultArray = { 0 };
+		return SmartDashboard.getRaw(PREFIX + key, defaultArray);
+	}
+}
+}
