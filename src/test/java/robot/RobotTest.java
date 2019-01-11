e09dc4fc9988607163dd17c1a1738dedfd833373
Noah Connors <noahconnors@mnmsa.org>
2019-01-10 19:57:54 -0600

    Add a dummy test

---
+++ src/test/java/robot/RobotTest.java
@@ -0,0 +1,12 @@
+package robot;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class RobotTest {
+    @Test
+    public void testDeadband() {
+        // This is a dummy test
+        assertTrue(true);
+    }
+}
