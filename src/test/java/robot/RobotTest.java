e89586b21c3498bc33333be7212660276ceff1b4
Noah Connors <noahconnors@mnmsa.org>
2019-01-11 20:43:34 -0600

    Add a deadband test function

--- src/test/java/robot/RobotTest.java
+++ src/test/java/robot/RobotTest.java
@@ -1,12 +1,22 @@
 package robot;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
-import static org.junit.Assert.assertTrue;
+import frc.robot.Robot;
 
 public class RobotTest {
     @Test
     public void testDeadband() {
-        // This is a dummy test
-        assertTrue(true);
+        final double delta = 0.00001;
+        assertEquals(-0, Robot.deadband(0, 0.2), delta);
+        assertEquals(-1, Robot.deadband(-1, 0.2), delta);
+        assertEquals(1, Robot.deadband(1, 0.2), delta);
+        assertEquals(0, Robot.deadband(0.2, 0.2), delta);
+        assertEquals(0, Robot.deadband(-0.2, 0.2), delta);
+        assertEquals(0, Robot.deadband(0.1, 0.2), delta);
+        assertEquals(0, Robot.deadband(-0.1, 0.2), delta);
+        assertEquals(0.5, Robot.deadband(0.6, 0.2), delta);
+        assertEquals(-0.5, Robot.deadband(-0.6, 0.2), delta);
     }
 }
