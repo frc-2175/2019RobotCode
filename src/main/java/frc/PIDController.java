851d7fb173f383077a6603840f819ecd5106d317
Noah Connors <noahconnors@mnmsa.org>
2019-01-15 17:08:49 -0600

    Add a PID test and fix the PID class

--- src/main/java/frc/PIDController.java
+++ src/main/java/frc/PIDController.java
@@ -2,33 +2,33 @@ package frc;
 
 public class PIDController {
     // lastUpdated is measured in miliseconds
-    double kp, ki, kd, integral, lastUpdated, previousError;
+    double kp, ki, kd, integral, lastUpdated, previousError, dt;
     public PIDController(double p, double i, double d) {
         this.kp = p;
         this.ki = i;
         this.kd = d;
         integral = 0;
         previousError = Double.NaN;
+        dt = 0;
     }
 
     public void clear() {
+        dt = 0;
         integral = 0;
-        previousError = 0;
-        lastUpdated = System.currentTimeMillis();
+        previousError = Double.NaN;
     }
 
     public double pid(double input, double setpoint) {
         double error = setpoint - input;
         double p = error * kp;
-        integral += getTimeSinceLastUpdated() * error;
+        integral += dt * error;
         double i = integral * ki;
-        double d = previousError == Double.NaN ? 0 : ((error - previousError) / getTimeSinceLastUpdated()) * kd;
-        lastUpdated = System.currentTimeMillis();
+        double d = Double.isNaN(previousError) ? 0 : ((error - previousError) / dt) * kd;
         previousError = error;
         return p + i + d;
     }
-
-    private double getTimeSinceLastUpdated() {
-        return (System.currentTimeMillis() - lastUpdated) / 1000.0;
+    
+    public void updateTime(double dt) {
+        this.dt = dt;
     }
 }
