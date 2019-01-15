136bba34e049e64935c5a69d7f98f84c1bfca70f
Noah Connors <noahconnors@mnmsa.org>
2019-01-15 17:20:48 -0600

    Document the PID Controller class

--- src/main/java/frc/PIDController.java
+++ src/main/java/frc/PIDController.java
@@ -1,8 +1,14 @@
 package frc;
 
 public class PIDController {
-    // lastUpdated is measured in miliseconds
-    double kp, ki, kd, integral, lastUpdated, previousError, dt;
+    double kp, ki, kd, integral, previousError, dt;
+
+    /**
+     * Constructs a new pid controller with constants
+     * @param p proportional gain
+     * @param i integral gain
+     * @param d derivative gain
+     */
     public PIDController(double p, double i, double d) {
         this.kp = p;
         this.ki = i;
@@ -12,12 +18,22 @@ public class PIDController {
         dt = 0;
     }
 
+    /** 
+     * At any time you want to re-use a PID controller with new
+     * constants, call this method.
+     */
     public void clear() {
         dt = 0;
         integral = 0;
         previousError = Double.NaN;
     }
 
+    /**
+     * Runs a step of the PID loop with a certain input and setpoint
+     * @param input the input to the controller
+     * @param setpoint the desired setpoint
+     * @return the output of the loop
+     */
     public double pid(double input, double setpoint) {
         double error = setpoint - input;
         double p = error * kp;
@@ -28,6 +44,12 @@ public class PIDController {
         return p + i + d;
     }
     
+    /**
+     * Every time the loop goes forward by one interation, call this
+     * method with a new dt.
+     * @param dt change in time from the last time this PID controller
+     * was run
+     */
     public void updateTime(double dt) {
         this.dt = dt;
     }
