b99b853079127035234ae4188ac70f4768b96822
Erik Nelson <eriknelson@mnmsa.org>
2019-02-14 22:31:21 -0600

    Fix property file deployment

--- deployPropertyFiles_Practice.bat
+++ deployPropertyFiles_Practice.bat
@@ -1,6 +1,6 @@
 ::Shared Files
-echo y | pscp -pw "" src\properties\*.properties admin@roborio-2175-frc.local:/home/lvuser
+echo y | pscp -pw "" src\Properties\*.properties admin@10.21.75.2:/home/lvuser
 ::Bot Specified Files
-echo y | pscp -pw "" src\properties\practiceBot\*.properties admin@roborio-2175-frc.local:/home/lvuser
+echo y | pscp -pw "" src\Properties\practiceBot\*.properties admin@10.21.75.2:/home/lvuser
 ::SSH files to robot
 echo y | plink -ssh -pw "" admin@roborio-2175-frc.local "mkdir -m 775 -v -p log; . /etc/profile.d/natinst-path.sh; /usr/local/frc/bin/frcKillRobot.sh -t -r"
