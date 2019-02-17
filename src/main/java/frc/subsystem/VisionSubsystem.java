b66b8baaa271c1504da3dae0d3c02f9bef73728e
Noah Connors <noahconnors@mnmsa.org>
2019-02-17 12:55:15 -0600

    Create vision subsystem for getting values from limelight

--- src/main/java/frc/subsystem/VisionSubsystem.java
+++ src/main/java/frc/subsystem/VisionSubsystem.java
@@ -1,60 +1,66 @@
 package frc.subsystem;
 
-import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import frc.ServiceLocator;
+import frc.Vector;
 
 public class VisionSubsystem {
-	private static NetworkTableInstance table = null;
-
-	public static enum LightMode {
-		eOn, eOff, eBlink
+	private final NetworkTable limelightTable = NetworkTableInstance.getDefault().getTable("limelight");
+	public VisionSubsystem() {
+		ServiceLocator.register(this);
 	}
 
-	public static enum CameraMode {
-		eVision, eDriver
+	/**
+	 * @return horizontal angle offset
+	 */
+	public double getHorizontalAngleOffset() {
+		return limelightTable.getEntry("tx").getDouble(0.0);
 	}
 
-	public static boolean isTarget() {
-		return getValue("tv").getDouble(0) == 1;
+	/**
+	 * @return vertical angle offset
+	 */
+	public double getVerticalAngleOffset() {
+		return limelightTable.getEntry("ty").getDouble(0.0);
 	}
 
-	public static double getTx() {
-		return getValue("tx").getDouble(0.00);
+	/**
+	 * @return whether limelight has any valid targets
+	 */
+	public boolean doesValidTargetExist() {
+		return limelightTable.getEntry("tv").getDouble(0.0) >= 1;
 	}
 
-	public static double getTy() {
-		return getValue("ty").getDouble(0.00);
+	/**
+	 * @param pipelineNumber pipeline index of camera you want (0-9)
+	 */
+	public void setPipeline(int pipelineNumber) {
+		limelightTable.getEntry("pipeline").setNumber(pipelineNumber);
 	}
 
-	public static double getTa() {
-		return getValue("ta").getDouble(0.00);
+	/**
+	 * @param camModeNumber number for camera mode you want (0 vision processor, 1 driver cam)
+	 */
+	public void setCamMode(int camModeNumber) {
+		limelightTable.getEntry("camMode").setNumber(camModeNumber);
 	}
 
-	public static double getTs() {
-		return getValue("ts").getDouble(0.00);
+	/**
+	 * @param ledModeNumber number for LED mode (0 use current LED mode, 1 force off, 2 force blink, 3 force on)
+	 */
+	public void setLedMode(int ledModeNumber) {
+		limelightTable.getEntry("ledMode").setNumber(ledModeNumber);
 	}
 
-	public static double getTl() {
-		return getValue("tl").getDouble(0.00);
-	}
-
-	public static void setLedMode(LightMode mode) {
-		getValue("ledMode").setNumber(mode.ordinal());
-	}
-
-	public static void setCameraMode(CameraMode mode) {
-		getValue("camMode").setNumber(mode.ordinal());
-	}
-
-	public static void setPipeline(int number) {
-		getValue("pipeline").setNumber(number);
-	}
-
-	private static NetworkTableEntry getValue(String key) {
-		if (table == null) {
-			table = NetworkTableInstance.getDefault();
+	public Vector[] getCorners() {
+		double[] defaultCornerArray = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
+		double[] cornerXCoordinates = limelightTable.getEntry("tcornx").getDoubleArray(defaultCornerArray);
+		double[] cornerYCoordinates = limelightTable.getEntry("tcorny").getDoubleArray(defaultCornerArray);
+		Vector[] corners = new Vector[cornerXCoordinates.length];
+		for (int i = 0; i < cornerXCoordinates.length; i++){
+			corners[i] = new Vector(cornerXCoordinates[i], cornerYCoordinates[i]);
 		}
-
-		return table.getTable("limelight").getEntry(key);
+		return corners;
 	}
 }
